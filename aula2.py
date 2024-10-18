67cacc98e006bef52e279a667ee5289050ef6cd5
Luiz gabriel <117752740+LuizGBVieira@users.noreply.github.com>
2024-10-18 08:35:12 -0300

    Delete aula2.py

--- aula2.py
+++
@@ -1,7 +0,0 @@
-primeiro_valor = input('Digite um nuemro:')
-segundo_valor = input('Digite um outro numero:')
-
-if primeiro_valor <= segundo_valor:
-    print(f'O valor de {primeiro_valor} é menor ou igual a {segundo_valor}')
-else:
-    print(f'O valor de {primeiro_valor} é maior que {segundo_valor}')
